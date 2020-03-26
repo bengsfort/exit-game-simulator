3e3df0bbf5d3d62a17c721b3936e8e6705fe1eda
Matt Bengston <matt.bengston@epicgames.com>
2020-03-26 17:15:05 +0200

    working ws test

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,9 +1,24 @@
 import express from "express";
+import WebSocket from "ws";
+
 import { join } from "path";
+
+// @todo: Would be cool to do our own implementation:
+// @see https://datatracker.ietf.org/doc/rfc6455/?include_text=1
+// @see https://developer.mozilla.org/en-US/docs/Web/API/WebSockets_API/Writing_WebSocket_servers
 
 const app = express();
 const port = 3000;
 
 app.use("/", express.static(join(__dirname, "public")));
 
-app.listen(port, () => console.log(`Server up and running on ${port}`));
+const server = app.listen(port, () =>
+  console.log(`Server up and running on ${port}`)
+);
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", ws => {
+  ws.on("open", () => console.log("Connection opened"));
+  ws.on("message", data => console.log("Message from client:", data));
+  ws.send("Connected.");
+});
